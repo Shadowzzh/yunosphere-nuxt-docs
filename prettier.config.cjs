012e565075bc3845c24bcae213760d01a7ce7e6d
ziheng <shadow1746556951@gmail.com>
2024-12-23 22:30:40 +0800

    chore: prettier Synchronization

---
+++ prettier.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+const config = {
+  // 是否使用分号
+  semi: true,
+  // 是否使用单引号
+  singleQuote: true,
+  // 缩进宽度
+  tabWidth: 2,
+  // 是否使用尾逗号
+  trailingComma: 'es5',
+  // 打印宽度
+  printWidth: 100,
+  // 是否在对象和数组字面量中打印空格
+  bracketSpacing: true,
+  // 是否在文件末尾打印换行符
+  endOfLine: 'lf',
+  // 是否在对象和数组字面量中打印空格
+  bracketSpacing: true,
+};
+
+module.exports = config;
